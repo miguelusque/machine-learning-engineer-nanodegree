7bc709772872d369e53a3d3553e816716f757789
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2018-03-13 21:03:02 +0100

    Solved question 7

--- 04_smartcab/smartcab/agent.py
+++ 04_smartcab/smartcab/agent.py
@@ -25,6 +25,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
+        self.trial = 1
 
 
     def reset(self, destination=None, testing=False):
@@ -43,7 +44,12 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = self.epsilon - 0.05
+            #self.epsilon = self.epsilon - 0.05
+            #self.epsilon = 1/(self.trial**2)
+            #self.epsilon = self.alpha ** self.trial
+            self.epsilon = math.exp(-self.alpha * self.trial)
+            self.trial += 1
+
 
         return None
 
@@ -143,7 +149,6 @@ class LearningAgent(Agent):
 
         return
 
-
 def run():
     """ Driving function for running the simulation.
         Press ESC to close the simulation, or [SPACE] to pause the simulation. """
@@ -162,7 +167,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.005)
 
     ##############
     # Follow the driving agent
@@ -177,14 +182,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
 
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=50, tolerance=0.01)
 
 
 if __name__ == '__main__':
